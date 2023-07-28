82f5de7f9b80994461a7b46a2434303bdc0802a5
Nix Young <impactresonance+tech@gmail.com>
2023-07-28 18:27:06 -0400

    updating vending machine yet again

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -16,9 +16,12 @@ public class VendingMachineCLI {
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String MAIN_MENU_EXIT = "Exit";
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
-	private List<Inventory> inventoryList = new ArrayList<>();
 	private static final Scanner scanner = new Scanner(System.in);
-	private static int defaultItemQuantity = 5;
+
+	private InventoryManager inventoryManager = new InventoryManager();
+	private PurchaseMenuDisplay purchaseMenu = new PurchaseMenuDisplay();
+
+//	private static int currentMoney = 0;
 
 	public static void main(String[] args) {
 		VendingMachineCLI cli = new VendingMachineCLI();
@@ -40,51 +43,28 @@ public class VendingMachineCLI {
 				case "1": // display items
 					System.out.println();
 					System.out.println("Displaying items.");
-					createInventory();
+					inventoryManager.createInventory();
 					break;
 				case "2": // purchase
 					System.out.println();
 					System.out.println("Displaying Purchase Menu.");
+					purchaseMenu.purchaseMenu();
 					break;
 				case "3": // exit
 					System.out.println();
 					System.out.println("Thank you for your patronage. Goodbye!");
 					System.exit(1);
+					break;
 				default: // catch incorrect input
 					System.out.println();
 					System.out.println("Please input a value 1 to 3.");
-					userInput = scanner.nextLine();
+					scanner.nextLine();
 					break;
 			}
 			System.out.println();
 			System.out.println("Press any key to return to the main menu.");
-			userInput = scanner.nextLine();
+			scanner.nextLine();
 		}
 	}
 
-	public void createInventory() {
-		File inventoryInput = new File("main.csv");
-
-		try (Scanner scanner = new Scanner(inventoryInput)) {
-			// adding item to array
-			while (scanner.hasNextLine()) {
-				String item = scanner.nextLine();
-				String[] itemArray = item.split(",");
-				this.inventoryList.add(new Inventory(itemArray[0], itemArray[1], Double.parseDouble(itemArray[2]),
-						itemArray[3], defaultItemQuantity));
-			}
-			// looping through items to print
-			for (Inventory inventory : inventoryList) {
-				System.out.println(inventory.getItemLocation() + "\t" + inventory.getItemName() + "\t"
-						+ inventory.getItemPrice() + "\t" + inventory.getItemType() + "\t"
-						+ inventory.getItemQuantity());
-			}
-		} catch (FileNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	public void purchaseItem() {
-
-	}
 }

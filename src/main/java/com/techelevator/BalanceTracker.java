5a27b0d1bcc6ec08484095ada35323445be196fe
Amanda Schaffer <amrschaffer@gmail.com>
2023-07-28 12:32:02 -0400

    Amanda strikes again! - day 2

--- src/main/java/com/techelevator/BalanceTracker.java
+++ src/main/java/com/techelevator/BalanceTracker.java
@@ -1,12 +1,20 @@
 package com.techelevator;
 
-public class BalanceTracker extends FeedMoney
-{
+import java.util.Scanner;
+
+public class BalanceTracker {
     // Variables
+    // this keeps the amount
     private double currentBalance = 0.00;
+
+    // used to reset bal to 0 after done in menu 2
+    private double startingBalance = 0.00;
+
     // money subtracted is accounted for as PROD PRICE : TOTAL BAL updated after Dispense
     private double amountSubtracted;
-    // ADDs to TOTAL BAL : FeedMoney class implements this
+
+    // variable
+    private int amountInput;
 
 
     // getters
@@ -20,19 +28,52 @@ public class BalanceTracker extends FeedMoney
         return amountSubtracted;
     }
 
-    // setters
-    // retrieve and set TOTAL with FeedMoney amount
-    public void setCurrentBalance(double currentBalance)
+    public int getAmountInput()
     {
-        this.currentBalance = getCurrentBalance() + getAmountInput();
+        // while the user would like to continue to deposit money
+        boolean isUserDone = false;
+
+        while (isUserDone) {
+            // prompt user to enter an INT for dollar amount
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter a dollar amount, as a whole number, that you would like to deposit: ");
+            String userInputForDeposit = input.nextLine();
+
+            int userDepositAsInt = Integer.parseInt(userInputForDeposit);
+
+            amountInput += userDepositAsInt;
+
+            System.out.println("Would you like to exit? Enter YES to exit, NO to continue: ");
+            String userInputToExit = input.nextLine();
+
+            if (userInputToExit.equalsIgnoreCase("YES"))
+            {
+                isUserDone = true;
+            }
+        }
+        return amountInput;
     }
 
-    // retrieve and set to PRICE
+    // setters
+    public void setCurrentBalanceAdded()      // FIGURE OUT WHERE/HOW TO CALL/IMPLEMENT
+    {
+        this.currentBalance = getCurrentBalance() + amountInput;
+    }
+
+    public void setCurrentBalanceSubtracted()     // subtract after each product dispense
+    {
+        this.currentBalance = getCurrentBalance() - getAmountSubtracted();
+    }
+
+    // retrieve and set to PRICE -> NEED TO INPUT PURCHASE PRICE
     public void setAmountSubtracted(double amountSubtracted)
     {
         this.amountSubtracted = amountSubtracted;
     }
 
+    public void setCurrentToStartBalance()
+    {
+        this.currentBalance = startingBalance;
+    }
 
-    // need a way to exit when done putting in money
 }

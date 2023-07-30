53784810600faa495e9a1f3c7e957db822d1088c
Amanda Schaffer <amrschaffer@gmail.com>
2023-07-30 17:39:45 -0400

    Almost Done! Finish tests...

--- src/test/java/com/techelevator/InventoryTest.java
+++ src/test/java/com/techelevator/InventoryTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InventoryTest
 {
@@ -12,26 +14,47 @@ public class InventoryTest
     {
         // Arrange
         InventoryManager inventoryManager = new InventoryManager();
+        List<InventoryManager> inventoryList = new ArrayList<>();
 
         // Act
         inventoryManager.setItemLocation("B1");
+        String location = "B1";
 
         inventoryManager.setItemName("Apple");
+        String name = "Apple";
 
         inventoryManager.setItemPrice(BigDecimal.valueOf(1.25));
-        BigDecimal applePrice = BigDecimal.valueOf(1.25);
+        BigDecimal price = BigDecimal.valueOf(1.25);
+
+        inventoryManager.setItemType("Fruit");
+        String type = "Fruit";
 
         inventoryManager.setItemQuantity(5);
+        int quantity = 5;
 
-        inventoryManager.setItemType("fruit");
-
+        inventoryList.add(new InventoryManager(location, name, price, type, quantity));
 
         // Assert
-        Assert.assertEquals( "B1", inventoryManager.getItemLocation() );
-        Assert.assertEquals( "Apple", inventoryManager.getItemName() );
-        Assert.assertEquals( applePrice, inventoryManager.getItemPrice() );
-        Assert.assertEquals( 5, inventoryManager.getItemQuantity() );
-        Assert.assertEquals( "fruit", inventoryManager.getItemType() );
+        Assert.assertEquals( location, inventoryManager.getItemLocation() );
+        Assert.assertEquals( name, inventoryManager.getItemName() );
+        Assert.assertEquals( price, inventoryManager.getItemPrice() );
+        Assert.assertEquals( type, inventoryManager.getItemType() );
+        Assert.assertEquals( quantity, inventoryManager.getItemQuantity() );
 
     }
+
+    @Test
+    public void return_new_item_quantity()
+    {
+        // Arrange
+        InventoryManager inventoryManager = new InventoryManager();
+
+        // Act
+        inventoryManager.setItemQuantity(5);
+
+        // Assert
+        Assert.assertEquals(4, inventoryManager.subtractItemQuantity());
+
+    }
+
 }
